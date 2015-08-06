4597aa30c8fa95172263dd6360252a91f407127a
BitVoyage <zhanglusheng@outlook.com>
2015-08-06 14:02:04 +0800

    rewrite _List.c

--- MyList/_List.h
+++ MyList/_List.h
@@ -11,6 +11,7 @@ int IsLast(Position P, List L);
 Position Find(int X, List L);
 void Delete(int X, List L);
 Position FindPrevious(int X, List L);
+void Insert(int X, List L, Position P);
 void DeleteList(List L);
 
 #endif
